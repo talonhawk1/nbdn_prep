a77a102a536165097bbb9a65b3a749489aff6514
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2010-07-12 18:35:11 -0400

    Adding the criteriafactory

--- exercise_to_complete_for_day_1/product/nothinbutdotnetprep/utility/filtering/Where.cs
+++ exercise_to_complete_for_day_1/product/nothinbutdotnetprep/utility/filtering/Where.cs
@@ -1,11 +1,20 @@
 ﻿using System;
+using nothinbutdotnetprep.collections;
 
 namespace nothinbutdotnetprep.utility.filtering
 {
 
     public class Where<ItemToFilter>
     {
-        public static ?? has_a<PropertyType>(Func<ItemToFilter,PropertyType> accessor)
+        public static CriteriaFactory<ItemToFilter,PropertyType> has_a<PropertyType>(Func<ItemToFilter,PropertyType> accessor)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class CriteriaFactory<ItemToFilter, PropertyType>
+    {
+        public Criteria<ItemToFilter> equal_to(ProductionStudio pixar)
         {
             throw new NotImplementedException();
         }
